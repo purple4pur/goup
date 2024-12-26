084e6ca09a136b6d27f873e2fb99dd53a1443bd5
Purple4pur <purple4pur@gmail.com>
2024-12-27 01:27:17 +0800

    go fmt

--- envutil/envutil.go
+++ envutil/envutil.go
@@ -1,83 +1,83 @@
 package envutil
 
 import (
-    "errors"
-    "fmt"
-    "log"
-    "os"
-    "strconv"
-    "strings"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type Env struct {
-    ListenPort      string
-    TargetUrl       string
-    SensitiveHeader string
-    AllowedIdList   []int
+	ListenPort      string
+	TargetUrl       string
+	SensitiveHeader string
+	AllowedIdList   []int
 }
 
 func (e *Env) _getEnv(envEntry string) string {
-    res, _ := os.LookupEnv(envEntry)
-    return res
+	res, _ := os.LookupEnv(envEntry)
+	return res
 }
 
 func (e *Env) _getListenPort(envEntry string) string {
-    if res := e._getEnv(envEntry); res != "" {
-        return ":" + res
-    } else {
-        return ""
-    }
+	if res := e._getEnv(envEntry); res != "" {
+		return ":" + res
+	} else {
+		return ""
+	}
 }
 
 func (e *Env) _getAllowedIdList(envEntry string) []int {
-    a := e._getEnv(envEntry)
-    if a == "" {
-        return []int{}
-    }
-    aList := strings.Split(a, ",")
-    res := make([]int, len(aList), len(aList))
-    for i, s := range aList {
-        n, err := strconv.Atoi(s)
-        if err != nil {
-            log.Fatal(err)
-        }
-        res[i] = n
-    }
-    return res
+	a := e._getEnv(envEntry)
+	if a == "" {
+		return []int{}
+	}
+	aList := strings.Split(a, ",")
+	res := make([]int, len(aList), len(aList))
+	for i, s := range aList {
+		n, err := strconv.Atoi(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+		res[i] = n
+	}
+	return res
 }
 
 func (e Env) _validate() error {
-    if e.ListenPort == "" || e.TargetUrl == "" {
-        return errors.New("envutil.Env: no ListenPort or TargetUrl")
-    }
-    if e.SensitiveHeader == "" {
-        return errors.New("envutil.Env: no SensitiveHeader")
-    }
-    return nil
+	if e.ListenPort == "" || e.TargetUrl == "" {
+		return errors.New("envutil.Env: no ListenPort or TargetUrl")
+	}
+	if e.SensitiveHeader == "" {
+		return errors.New("envutil.Env: no SensitiveHeader")
+	}
+	return nil
 }
 
 func (e *Env) Init() error {
-    e.ListenPort = e._getListenPort("PORT")
-    e.TargetUrl = e._getEnv("TARGET")
-    e.SensitiveHeader = e._getEnv("SENSITIVE_HEADER")
-    e.AllowedIdList = e._getAllowedIdList("ALLOWED_ID")
+	e.ListenPort = e._getListenPort("PORT")
+	e.TargetUrl = e._getEnv("TARGET")
+	e.SensitiveHeader = e._getEnv("SENSITIVE_HEADER")
+	e.AllowedIdList = e._getAllowedIdList("ALLOWED_ID")
 
-    msg := fmt.Sprintf("[Env/Init] --------------------------------\n")
-    msg += fmt.Sprintf("Read and parsed from env:\n")
-    msg += fmt.Sprintf("  PORT             -> ListenPort      = %s\n", e.ListenPort)
-    msg += fmt.Sprintf("  TARGET           -> TargetUrl       = %s\n", e.TargetUrl)
-    msg += fmt.Sprintf("  SENSITIVE_HEADER -> SensitiveHeader = %s\n", e.SensitiveHeader)
-    msg += fmt.Sprintf("  ALLOWED_ID       -> AllowedIdList   = %+v\n", e.AllowedIdList)
-    log.Print(msg)
+	msg := fmt.Sprintf("[Env/Init] --------------------------------\n")
+	msg += fmt.Sprintf("Read and parsed from env:\n")
+	msg += fmt.Sprintf("  PORT             -> ListenPort      = %s\n", e.ListenPort)
+	msg += fmt.Sprintf("  TARGET           -> TargetUrl       = %s\n", e.TargetUrl)
+	msg += fmt.Sprintf("  SENSITIVE_HEADER -> SensitiveHeader = %s\n", e.SensitiveHeader)
+	msg += fmt.Sprintf("  ALLOWED_ID       -> AllowedIdList   = %+v\n", e.AllowedIdList)
+	log.Print(msg)
 
-    return e._validate()
+	return e._validate()
 }
 
 func NewEnv() *Env {
-    e := new(Env)
-    err := e.Init()
-    if err != nil {
-        panic(err)
-    }
-    return e
+	e := new(Env)
+	err := e.Init()
+	if err != nil {
+		panic(err)
+	}
+	return e
 }
--- main.go
+++ main.go
@@ -1,163 +1,163 @@
 package main
 
 import (
-    "bytes"
-    "encoding/binary"
-    "fmt"
-    "io"
-    "log"
-    "net/http"
-    "net/http/httputil"
-    "net/url"
-    "slices"
-    "strconv"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"slices"
+	"strconv"
 
-    envutil "github.com/purple4pur/goup/envutil"
+	envutil "github.com/purple4pur/goup/envutil"
 )
 
 type pHead struct {
-    Id     [3]byte
-    Length [4]byte
+	Id     [3]byte
+	Length [4]byte
 }
 
 func getIntFromLittleEndianBytes(s []byte) int {
-    var res int
-    _s := make([]byte, len(s), len(s))
-    _ = copy(_s, s)
-    slices.Reverse(_s)
-    for _, v := range _s {
-        res <<= 8
-        res |= int(v)
-    }
-    return res
+	var res int
+	_s := make([]byte, len(s), len(s))
+	_ = copy(_s, s)
+	slices.Reverse(_s)
+	for _, v := range _s {
+		res <<= 8
+		res |= int(v)
+	}
+	return res
 }
 
 func getLittleEndianBytesFromInt(n uint32) []byte {
-    res := make([]byte, 4, 4)
-    binary.LittleEndian.PutUint32(res, n)
-    return res
+	res := make([]byte, 4, 4)
+	binary.LittleEndian.PutUint32(res, n)
+	return res
 }
 
 func main() {
-    env := envutil.NewEnv()
+	env := envutil.NewEnv()
 
-    u, _ := url.Parse(env.TargetUrl)
-    log.Printf("Forwarding %s -> %s\n", env.ListenPort, env.TargetUrl)
+	u, _ := url.Parse(env.TargetUrl)
+	log.Printf("Forwarding %s -> %s\n", env.ListenPort, env.TargetUrl)
 
-    proxy := httputil.NewSingleHostReverseProxy(u)
-    proxy.ModifyResponse = func(r *http.Response) error {
-        b, err := io.ReadAll(r.Body)
-        if err != nil {
-            return err
-        }
-        defer r.Body.Close()
+	proxy := httputil.NewSingleHostReverseProxy(u)
+	proxy.ModifyResponse = func(r *http.Response) error {
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			return err
+		}
+		defer r.Body.Close()
 
-        if r.Header.Get(env.SensitiveHeader) != "" {
-            msg := ""
-            msg += fmt.Sprintf(">>> Get response, wanna inspect this...\n")
-            msg += fmt.Sprintf("Bytes length: %d\n", len(b))
-            if len(b) >= 8 {
-                msg += fmt.Sprintf("Dump: (0-7) % X\n", b[:8])
-            }
-            if len(b) >= 16 {
-                msg += fmt.Sprintf("     (8-15) % X\n", b[8:16])
-            }
-            if len(b) >= 24 {
-                msg += fmt.Sprintf("    (16-23) % X\n", b[16:24])
-            }
-            if len(b) >= 32 {
-                msg += fmt.Sprintf("    (24-31) % X\n", b[24:32])
-            }
-            msg += fmt.Sprintf("\n")
+		if r.Header.Get(env.SensitiveHeader) != "" {
+			msg := ""
+			msg += fmt.Sprintf(">>> Get response, wanna inspect this...\n")
+			msg += fmt.Sprintf("Bytes length: %d\n", len(b))
+			if len(b) >= 8 {
+				msg += fmt.Sprintf("Dump: (0-7) % X\n", b[:8])
+			}
+			if len(b) >= 16 {
+				msg += fmt.Sprintf("     (8-15) % X\n", b[8:16])
+			}
+			if len(b) >= 24 {
+				msg += fmt.Sprintf("    (16-23) % X\n", b[16:24])
+			}
+			if len(b) >= 32 {
+				msg += fmt.Sprintf("    (24-31) % X\n", b[24:32])
+			}
+			msg += fmt.Sprintf("\n")
 
-            shouldLift := false
-            var before int
-            var after int
-            var startIdx int
-            var endIdx int
-            var ph pHead
-            i := 0
+			shouldLift := false
+			var before int
+			var after int
+			var startIdx int
+			var endIdx int
+			var ph pHead
+			i := 0
 
-            for loop := true; loop; {
-                l, err := binary.Decode(b[i:], binary.LittleEndian, &ph)
-                if err != nil {
-                    msg += fmt.Sprintf("Read EOF. finish.\n")
-                    shouldLift = false
-                    break
-                }
-                i += l
-                pId := getIntFromLittleEndianBytes(ph.Id[:3])
-                pLen := getIntFromLittleEndianBytes(ph.Length[:4])
-                msg += fmt.Sprintf("Read packet: id=%d, len=%d\n", pId, pLen)
+			for loop := true; loop; {
+				l, err := binary.Decode(b[i:], binary.LittleEndian, &ph)
+				if err != nil {
+					msg += fmt.Sprintf("Read EOF. finish.\n")
+					shouldLift = false
+					break
+				}
+				i += l
+				pId := getIntFromLittleEndianBytes(ph.Id[:3])
+				pLen := getIntFromLittleEndianBytes(ph.Length[:4])
+				msg += fmt.Sprintf("Read packet: id=%d, len=%d\n", pId, pLen)
 
-                switch pId {
-                case 75:
-                    ver := make([]byte, pLen, pLen)
-                    l, _ := binary.Decode(b[i:i + pLen], binary.LittleEndian, &ver)
-                    i += l
-                    msg += fmt.Sprintf("-> (75) protocol: ver=%d\n", getIntFromLittleEndianBytes(ver))
-                case 5:
-                    id := make([]byte, pLen, pLen)
-                    l, _ := binary.Decode(b[i:i + pLen], binary.LittleEndian, &id)
-                    i += l
-                    idNum := getIntFromLittleEndianBytes(id)
-                    if idNum >= 0x8000_0000 { // negative number
-                        idNum = idNum - 0xFFFF_FFFF - 1
-                    }
-                    msg += fmt.Sprintf("-> (5) reply: id=%d\n", idNum)
-                    if slices.Contains(env.AllowedIdList, idNum) {
-                        shouldLift = true
-                        msg += fmt.Sprintf("-> YES, please lift me up!!!\n")
-                    } else {
-                        shouldLift = false
-                        loop = false
-                        msg += fmt.Sprintf("-> NO, I can't lift you...\n")
-                    }
-                case 71:
-                    mode := make([]byte, pLen, pLen)
-                    l, _ := binary.Decode(b[i:i + pLen], binary.LittleEndian, &mode)
-                    startIdx = i
-                    i += l
-                    endIdx = i
-                    before = getIntFromLittleEndianBytes(mode)
-                    msg += fmt.Sprintf("-> (71) client: mode=%d\n", before)
-                    loop = false
-                default:
-                    msg += fmt.Sprintf("-> (%d) don't care what it is. stop here.\n", pId)
-                    shouldLift = false
-                    loop = false
-                }
-                msg += fmt.Sprintf("\n")
-            }
+				switch pId {
+				case 75:
+					ver := make([]byte, pLen, pLen)
+					l, _ := binary.Decode(b[i:i+pLen], binary.LittleEndian, &ver)
+					i += l
+					msg += fmt.Sprintf("-> (75) protocol: ver=%d\n", getIntFromLittleEndianBytes(ver))
+				case 5:
+					id := make([]byte, pLen, pLen)
+					l, _ := binary.Decode(b[i:i+pLen], binary.LittleEndian, &id)
+					i += l
+					idNum := getIntFromLittleEndianBytes(id)
+					if idNum >= 0x8000_0000 { // negative number
+						idNum = idNum - 0xFFFF_FFFF - 1
+					}
+					msg += fmt.Sprintf("-> (5) reply: id=%d\n", idNum)
+					if slices.Contains(env.AllowedIdList, idNum) {
+						shouldLift = true
+						msg += fmt.Sprintf("-> YES, please lift me up!!!\n")
+					} else {
+						shouldLift = false
+						loop = false
+						msg += fmt.Sprintf("-> NO, I can't lift you...\n")
+					}
+				case 71:
+					mode := make([]byte, pLen, pLen)
+					l, _ := binary.Decode(b[i:i+pLen], binary.LittleEndian, &mode)
+					startIdx = i
+					i += l
+					endIdx = i
+					before = getIntFromLittleEndianBytes(mode)
+					msg += fmt.Sprintf("-> (71) client: mode=%d\n", before)
+					loop = false
+				default:
+					msg += fmt.Sprintf("-> (%d) don't care what it is. stop here.\n", pId)
+					shouldLift = false
+					loop = false
+				}
+				msg += fmt.Sprintf("\n")
+			}
 
-            if shouldLift {
-                msg += fmt.Sprintf("######## Lift U UP!!! ########\n")
-                msg += fmt.Sprintf("Before: mode=%d (% X)\n", before, getLittleEndianBytesFromInt(uint32(before)))
-                if (before & 0x1) == 1 {
-                    after = before | (1 << 2)
-                    as := getLittleEndianBytesFromInt(uint32(after))
-                    msg += fmt.Sprintf("After:  mode=%d (% X)\n", after, as)
-                    if len(as) == (endIdx - startIdx) { // final check
-                        b = slices.Replace(b, startIdx, endIdx, as...)
-                        msg += fmt.Sprintf("-> here you go! Dump: (%d-%d) % X\n", startIdx, endIdx - 1, b[startIdx:endIdx])
-                    } else {
-                        msg += fmt.Sprintf("-> something wrong here... I can't lift you.\n")
-                    }
-                } else {
-                    msg += fmt.Sprintf("-> wait aren't you a player? I can't lift you.\n")
-                }
-            }
+			if shouldLift {
+				msg += fmt.Sprintf("######## Lift U UP!!! ########\n")
+				msg += fmt.Sprintf("Before: mode=%d (% X)\n", before, getLittleEndianBytesFromInt(uint32(before)))
+				if (before & 0x1) == 1 {
+					after = before | (1 << 2)
+					as := getLittleEndianBytesFromInt(uint32(after))
+					msg += fmt.Sprintf("After:  mode=%d (% X)\n", after, as)
+					if len(as) == (endIdx - startIdx) { // final check
+						b = slices.Replace(b, startIdx, endIdx, as...)
+						msg += fmt.Sprintf("-> here you go! Dump: (%d-%d) % X\n", startIdx, endIdx-1, b[startIdx:endIdx])
+					} else {
+						msg += fmt.Sprintf("-> something wrong here... I can't lift you.\n")
+					}
+				} else {
+					msg += fmt.Sprintf("-> wait aren't you a player? I can't lift you.\n")
+				}
+			}
 
-            msg += fmt.Sprintf("<<< DONE\n\n\n\n")
-            log.Print("\n" + msg)
-        }
+			msg += fmt.Sprintf("<<< DONE\n\n\n\n")
+			log.Print("\n" + msg)
+		}
 
-        // make sure we set the body, and the relevant headers for well-formed clients to respect
-        r.Body = io.NopCloser(bytes.NewReader(b))
-        r.ContentLength = int64(len(b))
-        r.Header.Set("Content-Length", strconv.Itoa(len(b)))
-        return nil
-    }
+		// make sure we set the body, and the relevant headers for well-formed clients to respect
+		r.Body = io.NopCloser(bytes.NewReader(b))
+		r.ContentLength = int64(len(b))
+		r.Header.Set("Content-Length", strconv.Itoa(len(b)))
+		return nil
+	}
 
-    log.Fatal(http.ListenAndServe(env.ListenPort, proxy))
+	log.Fatal(http.ListenAndServe(env.ListenPort, proxy))
 }
